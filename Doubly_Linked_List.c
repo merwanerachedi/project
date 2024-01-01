e0b39d10595d9d3dee7e28f652822b78e93c1b29
RACHEDI Merwane Soltane Zakaria <merwane.rachedi@gmail.com>
2024-01-01 07:43:16 -1200

    thirs commit - changed nodes structures to add color, , started using 2dTextures, implemented buttons to work by steps, finalized the DLL creation process and visualisation

--- Doubly_Linked_List.c
+++ Doubly_Linked_List.c
@@ -2,39 +2,63 @@
 #include <stdlib.h>
 #include <raylib.h>
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
-#define SCREEN_WIDTH 1000
-#define SCREEN_HEIGHT 800
+// Button definition
+typedef struct Button {
+    Rectangle rec;
+    Color col;
+} Button;
+
+// Initializes button properties
+void init_button(Button *but, Rectangle rect, Color colr) {
+    but->rec = rect;
+    but->col = colr;
+}
+
+// Checks if the mouse is over the button
+bool is_mouse_over_button(Button but) {
+    return CheckCollisionPointRec(GetMousePosition(), but.rec);
+}
+
+// Checks if left mouse click is pressed on the button
+bool is_left_click_pressed(Button but) {
+    return (is_mouse_over_button(but) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT));
+}
+
+//noeud def
 
 struct Node {
     int data;
+    Color col;
     struct Node* prev;
     struct Node* next;
 };
 
+//noeud create
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     if (newNode == NULL) {
         printf("Memory allocation failed.\n");
         exit(EXIT_FAILURE);
     }
+    newNode->col = BLUE;
     newNode->data = data;
     newNode->prev = NULL;
     newNode->next = NULL;
     return newNode;
 }
 
-struct Node* createDoublyLinkedList(int size) {
+//DLL create set values
+struct Node* createDoublyLinkedList(int *tab,int size) {
     struct Node* head = NULL;
     struct Node* tail = NULL;
 
-    printf("Enter %d elements for the doubly linked list:\n", size);
+    
     for (int i = 0; i < size; ++i) {
-        int data;
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &data);
 
-        struct Node* newNode = createNode(data);
+        struct Node* newNode = createNode(tab[i]);
 
         if (head == NULL) {
             head = tail = newNode;
@@ -49,39 +73,61 @@ struct Node* createDoublyLinkedList(int size) {
     return head;
 }
 
+
+//DLL Create Random values
+struct Node* createRandomDoublyLinkedList(int size)
+{
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+    
+    for (int i = 0; i < size; ++i)
+    {
+        struct Node* newNode = createNode(GetRandomValue(0,100));
+
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        
+    }
+    tail->next = NULL;
+    return head;
+}
+
+
+//noeud represent
 void drawNode(struct Node* node, Vector2 pos, int nodeCount) {
     int rectWidth = SCREEN_WIDTH / (nodeCount * 2);
     int rectHeight = SCREEN_HEIGHT / 20;
 
-    DrawRectangle((int)pos.x - rectWidth / 2, (int)pos.y - rectHeight / 2, rectWidth, rectHeight, BLUE);
+    DrawRectangle((int)pos.x - rectWidth / 2, (int)pos.y - rectHeight / 2, rectWidth, rectHeight, node->col);
     DrawText(TextFormat("%d", node->data), (int)pos.x - MeasureText(TextFormat("%d", node->data), 20) / 2, (int)pos.y - 10, 20, BLACK);
 }
 
+//arrow represent
 void drawArrow(Vector2 start, Vector2 end) {
     DrawLineV(start, end, BLACK);
 
-   // Vector2 arrow[3] = { {end.x, end.y}, {end.x - 5, end.y - 5}, {end.x + 5, end.y - 5} };
-    //DrawTriangleLines(arrow[0], arrow[1], arrow[2], BLACK);
+
 }
-  
 
-void visualizeDoublyLinkedList(struct Node* head) {
-    int nodeCount = 0;
-    struct Node* temp = head;
+// fonction visu DLL
+void visualizeDoublyLinkedList(struct Node* head,RenderTexture2D canvas)
+{int nodeCount = 0;
+           struct Node* temp = head;
 
-    while (temp != NULL) {
-        nodeCount++;
-        temp = temp->next;
-    }
-
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Doubly Linked List Visualization");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        int count = 0;
+             while (temp != NULL) 
+             {
+                  nodeCount++;
+                  temp = temp->next;
+             }
+             BeginTextureMode(canvas);
+             ClearBackground(RAYWHITE);
+             
+             int count = 0;
         struct Node* current = head;
         Vector2 nodePosition = {SCREEN_WIDTH / (nodeCount + 1), SCREEN_HEIGHT / 2};
 
@@ -113,32 +159,254 @@ void visualizeDoublyLinkedList(struct Node* head) {
             current = current->next;
             count++;
         }
+        EndTextureMode();
+             
+       }
 
+
+
+
+int main() {
+    
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Input Visualization");
+    
+    RenderTexture2D target = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
+    
+    //init the rendertexture
+    BeginTextureMode(target);
+    ClearBackground(RAYWHITE);
+    EndTextureMode();
+    
+    SetTargetFPS(60);
+    
+    //button default dimesions
+    float buttonWidth = SCREEN_WIDTH * 0.4f;
+    float buttonHeight = SCREEN_HEIGHT * 0.2f;
+    
+    //button setup
+    Button button_0 = {0};
+    init_button(&button_0,(Rectangle){SCREEN_WIDTH / 2 - buttonWidth / 2 , SCREEN_HEIGHT / 2 - buttonHeight / 2 , buttonWidth , buttonHeight }, RED);
+    
+    Button button = { 0 };
+    init_button(&button, (Rectangle) { SCREEN_WIDTH / 2 - buttonWidth / 2, SCREEN_HEIGHT / 2 - buttonHeight / 2, buttonWidth, buttonHeight }, BLACK);
+    
+    Button button_random = { 0 };
+    init_button(&button_random,(Rectangle){SCREEN_WIDTH / 2 - buttonWidth * (3 / 2) , SCREEN_HEIGHT / 2 + buttonHeight / 2 , buttonWidth / 2 , buttonHeight }, RED);
+    
+    Button button_set = { 0 };
+    init_button(&button_set,(Rectangle){SCREEN_WIDTH / 2 + buttonWidth / 3 , SCREEN_HEIGHT / 2 + buttonHeight / 2  , buttonWidth / 2 , buttonHeight }, RED);
+    
+    // varibles de deroulement
+    bool step1 = true;
+    bool step2 = false;
+    bool step3 = false;
+    bool step4_1 = false;
+    bool step4_2 = false;
+    bool step5 = false;
+   
+    // Text input properties
+    int textSize = 0;
+    char text[64] = { 0 };
+    Vector2 textPosition = { SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 10 };
+    bool inputComplete = false;
+    int enteredNumber = 0;
+    
+    
+    static int* values;
+    struct Node* head;
+    int currentNode = 1;
+    
+    int nodeCount = 0;
+    struct Node* temp;
+
+ 
+ 
+
+
+    while (!WindowShouldClose()) {
+        
+        
+     //debut step1
+        if (step1)
+     {
+    BeginTextureMode(target);
+    ClearBackground(RAYWHITE);
+    DrawText("Welcome, press the button below to create a DLL.",SCREEN_WIDTH/2- MeasureText("donnez le nombre d'elements de la dll",20)+120,SCREEN_HEIGHT/3,20,BLACK);
+    DrawRectangleRec(button_0.rec,button_0.col);
+    DrawText("Create",button_0.rec.x + button_0.rec.width / 2 - MeasureText("Create",20) + 30,button_0.rec.y + button_0.rec.height / 2 - 20 / 2, 20 , BLACK );
+    
+    EndTextureMode();
+     }
+     
+     //deroulement des etapes
+     if(is_left_click_pressed(button_0)) 
+     { 
+     step1=false;
+     step2 = true;
+     }
+    
+       
+        if (!inputComplete) {
+            int key = GetKeyPressed(); // Get the pressed key 
+
+            if ((key >= 48 && key <= 57) && (textSize < 63)) { // Check if the key is a number and within text limit
+                text[textSize] = (char)key; // Store entered digit in the text array
+                textSize++;
+            }
+            else if (key == KEY_BACKSPACE && textSize > 0) { // Check for backspace to delete the last digit
+                textSize--;
+                text[textSize] = '\0'; // Null-terminate the string to erase the last character
+            }
+
+            if (IsKeyPressed(KEY_ENTER)) { // Clicking on ENTER will finalize the input
+                inputComplete = true;
+                enteredNumber = atoi(text); // Convert the entered text to an integer
+            }
+        }
+        
+        //debut step2
+        if(step2)
+       { 
+        BeginTextureMode(target);
+        ClearBackground(RAYWHITE);
+        DrawRectangleRec(button.rec, button.col);
+
+        DrawText(text, textPosition.x, textPosition.y, 40, WHITE); // Display the entered digits
+
+        if (!inputComplete) {
+            DrawText("Enter the size of the DLL.", SCREEN_WIDTH / 2 - MeasureText("Enter the size of the DLL.", 20) / 2, SCREEN_HEIGHT / 2 - 100 , 20, BLACK);
+            DrawText("Click on the ENTER key to finalize input.", SCREEN_WIDTH / 2 - MeasureText("Click on the ENTER key to finalize input.", 20) / 2, SCREEN_HEIGHT / 2 + 120, 20, BLACK);
+        }
+        else {
+            DrawText("Input Completed!", SCREEN_WIDTH / 2 - MeasureText("Input Completed!", 20) / 2, SCREEN_HEIGHT / 2 + 120, 20, BLACK);
+            DrawText("press on the SPACEBAR for the next step.", SCREEN_WIDTH / 2 - MeasureText("press on the SPACEBAR for the next step!", 20) / 2, SCREEN_HEIGHT / 2 + 160, 20, BLACK);
+            
+        }
+
+        EndTextureMode();
+        
+        }
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            step2 = false;
+            step3 = true;
+        }
+        
+        
+        //debut step3
+        if(step3 && !step1 && !step2)
+        {
+            BeginTextureMode(target);
+            ClearBackground(RAYWHITE);
+            
+            DrawText("select one of the choices below.", SCREEN_WIDTH / 2 - MeasureText("select one of the choices below.", 20) / 2, SCREEN_HEIGHT / 2 - 100 , 20, BLACK);
+        
+            DrawRectangleRec(button_random.rec, button_random.col);
+            DrawText("random values",button_random.rec.x + button_random.rec.width / 2 - MeasureText("random values",20) + 80,button_random.rec.y + button_random.rec.height / 2 - 20 / 2 ,20,BLACK);
+            
+            DrawRectangleRec(button_set.rec, button_set.col);
+            DrawText("pre set values",button_set.rec.x + button_set.rec.width / 2 - MeasureText("pre set values",20) + 80,button_set.rec.y + button_set.rec.height / 2 - 20 / 2 ,20,BLACK);
+            
+            
+            EndTextureMode();
+        }
+        
+        
+        
+        if(is_left_click_pressed(button_set))
+        {
+            step3 = false;
+            step4_1 = true;
+        }
+        if(is_left_click_pressed(button_random))
+        {
+            step3 = false;
+            step4_2 = true;
+        }
+        
+        
+        //debut step4_1
+       if(step4_1)
+       {
+           
+    BeginTextureMode(target);
+    ClearBackground(RAYWHITE);
+    DrawText("Enter values for the Doubly Linked List:", SCREEN_WIDTH / 2 - MeasureText("Enter values for the Doubly Linked List:", 20) / 2, SCREEN_HEIGHT / 4, 20, BLACK);
+    DrawText("Press ENTER after each input.", SCREEN_WIDTH / 2 - MeasureText("Press ENTER after each input.", 20) / 2, SCREEN_HEIGHT / 4 + 40, 20, BLACK);
+    EndTextureMode();
+
+    // Input values of nodes for the DLL
+      
+    
+    if (values == NULL) {
+        values = (int*)malloc(sizeof(int) * enteredNumber);
+        for (int i = 0; i < enteredNumber; ++i) {
+            values[i] = 0;
+        }
+    }
+
+    static char inputText[32] = "";
+    static int textSizeInput = 0;
+    static Vector2 textPositionInput = { SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 10 };
+
+    if (currentNode <= enteredNumber) {
+        if (!IsKeyDown(KEY_ENTER)) {
+            int key = GetKeyPressed();
+            if ((key >= 48 && key <= 57) && (textSizeInput < 31)) {
+                inputText[textSizeInput] = (char)key;
+                textSizeInput++;
+            }
+            else if (key == KEY_BACKSPACE && textSizeInput > 0) {
+                textSizeInput--;
+                inputText[textSizeInput] = '\0';
+            }
+        }
+        else if (IsKeyPressed(KEY_ENTER) && textSizeInput > 0) {
+            values[currentNode - 1] = atoi(inputText);
+            memset(inputText, 0, sizeof(inputText)); //init les valeurs du tab inputText a 0
+            textSizeInput = 0;
+            currentNode++;
+        }
+    }
+    else {
+         head = createDoublyLinkedList(values,enteredNumber);
+        step4_1 = false ;
+        step5 = true;
+    }
+           BeginTextureMode(target);
+           DrawRectangleRec(button.rec, button.col);
+           DrawText(inputText,textPositionInput.x,textPositionInput.y,40,GREEN);
+           EndTextureMode();
+       }
+       
+       //debut step 4_2
+       
+       if(step4_2)
+       {
+           head = createRandomDoublyLinkedList(enteredNumber);
+           step5 = true;
+           step4_2 = false;
+           
+       }
+       
+       
+       
+       //debut final step for creation
+       if(step5)
+       {
+           visualizeDoublyLinkedList(head,target);
+             
+       }
+
+        //debute main inter
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawTextureRec(target.texture, (Rectangle) { 0, 0, (float)target.texture.width, (float)-target.texture.height }, (Vector2) { 0, 0 }, WHITE);
         EndDrawing();
     }
 
+    
+
     CloseWindow();
-}
-
-void freeDoublyLinkedList(struct Node* head) {
-    struct Node* temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
-}
-
-int main() {
-    int size;
-    printf("Enter the size of the doubly linked list: ");
-    scanf("%d", &size);
-
-    struct Node* head = createDoublyLinkedList(size);
-
-    visualizeDoublyLinkedList(head);
-
-    freeDoublyLinkedList(head);
-
     return 0;
 }
